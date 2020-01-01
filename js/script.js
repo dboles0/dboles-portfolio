73e23beec04189db838affed3045de53678a546f
dboles0 <donald.c.boles@gmail.com>
2020-01-01 14:32:37 -0800

    changed student to applications developer

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 	});
 	
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		strings: ["Software Engineer.", "Web Developer.", "Applications Developer."],
 		typeSpeed: 70, 
 		loop: true, 
 		startDelay: 1000,
